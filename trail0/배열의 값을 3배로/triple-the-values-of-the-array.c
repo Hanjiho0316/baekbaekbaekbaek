de0d5065ba46c1668e35c19e16a1e50c6180a7e7
Hanjiho0316 <skhjh0316@gmail.com>
2026-05-31 14:05:11 +0900

    [Passed] 배열의 값을 3배로 (1ms, 0MB)

---
+++ trail0/배열의 값을 3배로/triple-the-values-of-the-array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arr1[3][3];
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%d ",arr1[i][j] * 3);
+        }
+        printf("\n");
+    }
+    return 0;
+}
