631663cc60ca9f24f09f4bf3d72fb920c3d05752
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-02 08:45:53 +0900

    [Passed] 실수와 문자 받아 출력하기 (1ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    float b,c;
+    scanf("%c %f %f",&a,&b,&c);
+    printf("%c\n%.2lf\n%.2lf",a,b,c);
+    return 0;
+}
