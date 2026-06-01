ff92119d5e0f1925b1d98bbd3c4c0d6fde90600f
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-02 08:41:41 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float a,b,c;
+    scanf("%f %f %f",&a,&b,&c);
+    printf("%.3lf\n%.3lf\n%.3lf",a,b,c);
+    return 0;
+}
