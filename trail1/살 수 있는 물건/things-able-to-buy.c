025a2678404ec6b8747f6fad9cb4521698dff5eb
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-04 19:34:57 +0900

    [Passed] 살 수 있는 물건 (1ms, 0MB)

---
+++ trail1/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    if(a >= 3000){
+        printf("book");
+    }else if(a>=1000){
+        printf("mask");
+    }else{
+        printf("no");
+    }
+    return 0;
+}
