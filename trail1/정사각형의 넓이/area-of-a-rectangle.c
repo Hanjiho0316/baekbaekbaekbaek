dfeb71d8af674c986c91f9f9ddf8c085b9167ecc
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-04 19:23:23 +0900

    [Passed] 정사각형의 넓이 (2ms, 0MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a*a);
+    if(a < 5){
+        printf("tiny");
+    }
+    return 0;
+}
