60f6d45b4b7d6043c71d4d020c1d25431824fa11
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-06 13:50:08 +0900

    [Passed] 4번의 크기 비교 (1ms, 0MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    if(a > b){
+        printf("1\n");
+    }else{
+        printf("0\n");
+    }
+    if(a > c){
+        printf("1\n");
+    }else{
+        printf("0\n");
+    }
+    if(a > d){
+        printf("1\n");
+    }else{
+        printf("0\n");
+    }
+    if(a > e){
+        printf("1\n");
+    }else{
+        printf("0\n");
+    }
+    return 0;
+}
