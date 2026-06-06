499ddb5b9797bdb094a0da280fbca38e5ccc320c
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-06 13:54:11 +0900

    [Passed] 시력 검사 (1ms, 0MB)

---
+++ trail1/시력 검사/eye-test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float a,b;
+    scanf("%f %f",&a,&b);
+    if(a >= 1 && b >= 1){
+        printf("High");
+    }else if(a >= 0.5 && b >= 0.5){
+        printf("Middle");
+    }else{
+        printf("Low");
+    }
+    return 0;
+}
