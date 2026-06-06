5a16228b9661139c81a6f945c78c371dc618b807
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-06 13:51:08 +0900

    [Passed] 점수 비교 (1ms, 0MB)

---
+++ trail1/점수 비교/score-comparison.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if(a > c && b > d){
+        printf("1\n");
+    }else{
+        printf("0\n");
+    }
+    return 0;
+}
