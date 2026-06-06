2e81397a77ab07228a85f092d5946a449845800c
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-06 14:14:25 +0900

    [Passed] 두 사람 (1ms, 0MB)

---
+++ trail1/두 사람/two-person.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    char c,d;
+    scanf("%d %c %d %c",&a,&c,&b,&d);
+    if((a >= 19 && c == 'M') ||(b >= 19 && d == 'M')){
+        printf("1");
+    }else{
+        printf("0");
+    }
+    return 0;
+}
