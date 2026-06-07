2c5d27c9516af191d700a3b945bfd08c83344dba
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-07 20:07:58 +0900

    [Passed] 문자열 출력하기 (1ms, 0MB)

---
+++ trail1/문자열 출력하기/print-string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    for(int i = 0; i < a; i++){
+        printf("LeebrosCode\n");
+    }
+    return 0;
+}
