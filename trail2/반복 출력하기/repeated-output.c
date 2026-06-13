f62e39a8e28ba58425fa9bc5f4cb903c5816e732
Hanjiho0316 <skhjh0316@gmail.com>
2026-06-13 15:02:41 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void exprint(int n){
+    for(int i = 0 ; i < n; i++){
+        printf("12345^&*()_\n");
+    }
+}
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    exprint(a);
+    return 0;
+}
